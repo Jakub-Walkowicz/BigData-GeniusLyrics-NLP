b4e9cab1543977bafd7fc3c44dc92c57e86538a9
walczykk <walkowiczjj@gmail.com>
2026-01-19 22:56:09 +0100

    Finished Logistic Regression

--- src/ml/logistic_regression_classifier.py
+++ src/ml/logistic_regression_classifier.py
@@ -1,26 +1,48 @@
 from pyspark.ml.classification import LogisticRegression
+from pyspark.ml.feature import StringIndexer, VectorAssembler, IndexToString
+from pyspark.ml import Pipeline
+from pyspark.ml.evaluation import MulticlassClassificationEvaluator
 
 class LogisticRegressionClassifier:
     def __init__(self, regParam=0.0, elasticNetParam=0.0, maxIter=10):
-        self.model = None
-        self.maxIter = maxIter
-        self.regParam = regParam
-        self.elasticNetParam = elasticNetParam
+        self.pipeline_model = None
+        self.params = {
+            "regParam": regParam,
+            "elasticNetParam": elasticNetParam,
+            "maxIter": maxIter
+        }
 
-    def fit(self, df, labelCol="tag", featuresCol="word_vectors"):
+    def fit(self, df, labelCol="tag", featuresCols=["word_vectors"]):        
+        indexer = StringIndexer(inputCol=labelCol, outputCol="tag_index")
+        
+        assembler = VectorAssembler(inputCols=featuresCols, outputCol="features")
+        
         lr = LogisticRegression(
-            labelCol=labelCol,
-            featuresCol=featuresCol,
-            maxIter=self.maxIter,
-            regParam=self.regParam,
-            elasticNetParam=self.elasticNetParam
+            labelCol="tag_index",
+            featuresCol="features",
+            **self.params
         )
-        self.model = lr.fit(df)
+        
+        label_converter = IndexToString(inputCol='prediction', outputCol="prediction_label", labels=indexer.fit(df).labels)
+        
+        pipeline = Pipeline(stages=[indexer, assembler, lr, label_converter])
+        self.pipeline_model = pipeline.fit(df)
+                
         return self
 
     def predict(self, df):
-        if not self.model:
+        if not self.pipeline_model:
             raise ValueError("Model must be fitted before transformation.")
-        df_transformed = self.model.transform(df)
+        df_transformed = self.pipeline_model.transform(df)
 
         return df_transformed
+
+    def evaluate_all(self, predictions, labelCol="tag")
+        evaluator = MulticlassClassificationEvaluator(
+            labelCol="tag_index",
+            predictionCol="prediction"
+        )
+        
+        metrics = ["accuracy", "f1", "weightedPrecision", "weightedRecall"]
+        
+        return {m: evaluator.setMetricName(m).evaluate(predictions) for m in metrics}
