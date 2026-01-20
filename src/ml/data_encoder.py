b6a3d1b6085c84ab5c6d225f87c8bffda4c146a3
walczykk <walkowiczjj@gmail.com>
2026-01-20 22:56:34 +0100

    Removed unused code, updated pdf export

--- src/ml/data_encoder.py
+++
@@ -1,7 +0,0 @@
-from pyspark.ml.feature import OneHotEncoder, StringIndexer 
-from pyspark.ml import Pipeline
-
-def encode_label(df, col: str):
-    indexer_output_col = col+"_index"
-    indexer = StringIndexer(inputCol=col, outputCol=indexer_output_col)
-    return indexer.fit(df).transform(df)
